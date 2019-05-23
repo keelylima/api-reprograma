1398f92b3f85c5898c3054beef9f8345e09091c9
Kelly Lima <keely06@live.com>
2019-05-22 22:27:33 -0300

    alteração no index/form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 const container = document.querySelector('.maravilhosas__box');
+const btn = document.querySelector('#btn');
 
-fetch('https://theblackwomanhistory.firebaseio.com/.json')
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    console.log(name);
+    const img = document.querySelector('#img').value;
+    console.log(img);
+
+    fetch('http://localhost:5001/maravilhosas', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'title': name,
+            'metadata': {
+                'image': {
+                    'url': img,
+                }
+            }
+        }),
+    })
+})
+
+fetch('http://localhost:5001/maravilhosas')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
 
         data.content.forEach((mulher) => {
-
-            // console.log(mulher.metadata);
 
             let box = document.createElement('div');
             box.setAttribute('class', 'maravilhosas__perfil');
@@ -21,12 +46,17 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
             let img = document.createElement('img');
             img.setAttribute('class', 'img-responsive');
 
-            if(mulher.metadata && mulher.metadata.image){
-                img.setAttribute('src', mulher.metadata.image.url);
-            } else {
-                img.setAttribute('src', './img/img-mulher.png');
-            }
+            mulher.metadata && mulher.metadata.image ? img.setAttribute('src', mulher.metadata.image.url) : img.setAttribute('src', './img/img-mulher.png');
 
+            // SEGUNDO
+            // if(mulher.metadata && mulher.metadata.image){
+            //     img.setAttribute('src', mulher.metadata.image.url);
+            // } else {
+            //     img.setAttribute('src', './img/img-mulher.png');
+            // }
+
+            
+            // PRIMEIRO
             // if (mulher.metadata) {
             //     if (mulher.metadata.image) {
             //         if (mulher.metadata.image.url) {
@@ -46,6 +76,7 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
             ancora.appendChild(nome);
 
         })
+
     })
     .catch(function (erro) {
         console.log(erro);
@@ -62,7 +93,7 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
 
 
 
-    
+
             // function mulherMetaData() {
             //     if (mulher.metadata) {
             //       return mulher.metadata;
@@ -73,7 +104,7 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
 
             // function mulherMetaDataImg(){
             //     if(mulher.metadata.image){
-                    
+
             //         return mulher;
             //     } else{
             //     img.setAttribute('src', './img/img-mulher.png');
