c8ca320a0d099d6774ac935279b031499305c546
Kelly Lima <keely06@live.com>
2019-05-23 22:19:11 -0300

    criação do delete

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ fetch('http://localhost:5001/maravilhosas')
 
             let box = document.createElement('div');
             box.setAttribute('class', 'maravilhosas__perfil');
+            // box.setAttribute('id', mulher.id);
             container.appendChild(box);
 
             let ancora = document.createElement('a');
@@ -51,6 +52,30 @@ fetch('http://localhost:5001/maravilhosas')
             let nome = document.createElement('p');
             nome.innerHTML = `${mulher.title}`;
             ancora.appendChild(nome);
+
+            let btn__2 = document.createElement('button');
+            btn__2.innerHTML = '✖';
+            btn__2.setAttribute('data-id', mulher.id);
+            box.appendChild(btn__2);
+
+            btn__2.addEventListener('click', () => {
+                fetch('http://localhost:5001/maravilhosas', {
+                    method: 'DELETE',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        'id': btn__2.getAttribute('data-id')
+                    })
+                })
+                .then(() => {
+                    box.remove();
+                })
+                .catch((erro) => {
+                    console.log(erro)
+                })
+            });
 
         })
 
